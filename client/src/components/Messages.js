e3483447b341c24cf5b69b0ff6536b6008cda8c4
kaylarubin <kayla.rose12@yahoo.com>
2022-10-13 20:05:25 -0600

    added cors library and a couple more emits events

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -1,11 +1,9 @@
-import React, { useRef } from "react";
+import React from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
 import Message from "./Message";
 import "../styles/Messages.css";
 
 const Messages = ({ messages, name }) => {
-  const scrollToBottomRef = useRef(null);
-
   return (
     <ScrollToBottom className="messages" mode={"bottom"}>
       {messages.map((message, i) => {
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
+const cors = require("cors");
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.js");
 
@@ -39,20 +40,36 @@ io.on("connection", (socket) => {
     //Call join to subscribe the socket to a given channel
     socket.join(user.room);
 
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
     callback();
   });
 
   socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id);
     io.to(user.room).emit("message", { user: user.name, text: message });
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
     callback();
   });
 
   socket.on("disconnect", () => {
-    console.log("User has left");
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name}, has left.`,
+      });
+    }
   });
 });
 
 app.use(router);
+app.use(cors());
 
 server.listen(PORT, () => console.log(`Server has started on port: ${PORT}`));
